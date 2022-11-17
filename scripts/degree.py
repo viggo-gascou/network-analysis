34c715fd59552fc2750b90c06450a3a3a8a84f15
viggogascou <94069687+viggo-gascou@users.noreply.github.com>
2022-11-17 11:00:12 +0000

    auto commit and push from Deepnote

--- scripts/degree.py
+++ scripts/degree.py
@@ -1,28 +1,35 @@
-num_degrees = Counter(node_list_degrees["Degree"]) 
+from collections import Counter
+import pandas as pd 
+import matplotlib.pyplot as plt
 
-# Normalising y with the total number of nodes in the network. 
+def make_df(node_list_degrees):
+    num_degrees = Counter(node_list_degrees["Degree"]) 
 
-degrees = num_degrees.keys()
-counts = num_degrees.values()
+    # Normalising y with the total number of nodes in the network. 
 
+    degrees = num_degrees.keys()
+    counts = num_degrees.values()
 
-pw = pd.DataFrame()
-pw["k"] = degrees
-pw["count"] = counts
+    pw = pd.DataFrame()
+    pw["k"] = degrees
+    pw["count"] = counts
 
-num_degrees_normalised = {key: value / len(G.nodes) for key, value in num_degrees.items()} 
-pw['p(k)'] = num_degrees_normalised.values() 
+    num_degrees_normalised = {key: value / len(degrees) for key, value in num_degrees.items()} 
+    pw['p(k)'] = num_degrees_normalised.values() 
+    return pw
 
 
 # plot the degrees against the number of nodes 
-
 # With log-scale
+def plot_degree_log(df):
+    df.plot(x = "k", y = "p(k)", kind = "scatter", loglog= True)
+    plt.title("Log-log plot of the Degree Distribution")
 
-pw.plot(x = "k", y = "p(k)", kind = "scatter", loglog= True)
+def plot_degree(df):
+    df.plot(x = "k", y = "p(k)", kind = "scatter" )
+    plt.title("Plot of the Degree Distribution")
 
-# without log-scale
 
-pw.plot(x = "k", y = "p(k)", kind = "scatter" )
 
 
 
